801415895bca864e5bdfc491873ab884ebbe5fc8
David Rooney <david_rooney16@hotmail.com>
2021-06-23 14:36:24 +0100

    Update MainContent.js
    Add google adsense into Head of all pages becuase it's crap and cant find the code in the website without it

--- src/core/MainContent.js
+++ src/core/MainContent.js
@@ -3,6 +3,16 @@ import CustomRouter from "./CustomRouter";
 function MainContent() {
   return (
       <div className="MainContent">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{siteDataState.IntroText}</title>
+          <link rel="canonical" href={siteDataState.WebsiteURL} />
+          <meta
+            name="description"
+            content={siteDataState.SiteDesctiption}
+          />
+          <script data-ad-client={process.env.REACT_APP_GOOGLE_ADSENSE_CLIENT_ID} async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        </Helmet>
         <CustomRouter />
       </div>
   );
